28f6ec3083e9c8c46b7a866e2d702ec00d96c1cb
NoeCampos22 <ama-noe@outlook.com>
2019-05-28 21:36:21 -0500

    TA_Class

---
+++ 3_API_Congnitive_Services/Clase/Main.py
@@ -0,0 +1,3 @@
+from TextAnalytics import TextAnalytics
+
+TA_Instancia = TextAnalytics("8c5fb6709dc0410384e8fdb8da42a76f", "eastus")
--- 3_API_Congnitive_Services/Clase/TextAnalytics.py
+++ 3_API_Congnitive_Services/Clase/TextAnalytics.py
@@ -1,2 +1,32 @@
 from azure.cognitiveservices.language.textanalytics import TextAnalyticsClient
 from msrest.authentication import CognitiveServicesCredentials
+
+class TextAnalytics():
+
+    def __init__(self, SubscriptionKey, Location):
+        self.SubscriptionKey = SubscriptionKey
+        self. TA_URL = "https://{0}.api.cognitive.microsoft.com/".format(Location)
+
+        self.Credentials = CognitiveServicesCredentials(SubscriptionKey)
+
+        self.Text_Analytics = TextAnalyticsClient(endpoint = self.TA_URL
+        , credentials = self.Credentials)
+
+    
+    def detectLanguages(self, Documents):
+        arrLanguages = self.Text_Analytics.detect_language(documents = Documents)
+
+        return arrLanguages
+
+    def analyzeSentiment(self, Documents):
+        arrScore = self.Text_Analytics.sentiment(documents = Documents)
+
+        return arrScore
+
+    def keyPhrases(self, Documents):
+        matKeyPhrases = self.Text_Analytics.key_phrases(documents = Documents)
+
+        return matKeyPhrases
+
+    def identifyEntites(self, Documents):
+        matEntitites = self.Text_Analytics.entities(documents = Documents)
